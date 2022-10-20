85a90ed051630e588779c7948515b4665365c11b
_Allan <allankipchumba76@gmail.com>
2022-10-20 08:44:58 +0300

    written code for footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./Footer.module.scss";
 
 export const Footer = () => {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <div className={styles.footer}>
+      &copy; {new Date().getFullYear()} All Rights Reserved
+    </div>
   );
 };
