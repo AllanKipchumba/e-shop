95ffdea94833c422ce4c78e5332fa2238f855388
_Allan <allankipchumba76@gmail.com>
2022-10-21 05:45:54 +0300

    written code for register page

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import styles from "./Auth.module.scss";
+import registerImg from "../../assets/register.png";
+import { Card } from "../../components/card/Card";
+import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
 
 export const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <h2>Register</h2>
+          <form>
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <input type="password" placeholder="Confirm password" required />
+
+            <button className="--btn --btn-primary --btn-block">
+              Register
+            </button>
+          </form>
+
+          <span className={styles.register}>
+            <p>Already have an account?</p>
+            <Link to="/login">Login</Link>
+          </span>
+        </div>
+      </Card>
+      <div className={styles.img}>
+        <img src={registerImg} alt="register" width="400" />
+      </div>
+    </section>
+  );
 };
