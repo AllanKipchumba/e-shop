46c2cc6117380201f9579d621b296e5056ef03fc
_Allan <allankipchumba76@gmail.com>
2022-12-15 18:15:55 +0300

    created Admin navbar

--- src/components/admin/navbar/Navbar.jsx
+++ src/components/admin/navbar/Navbar.jsx
@@ -1,7 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./navbar.module.scss";
 import { FaUserCircle } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+
+//style active link
+const activeLink = ({ isActive }) => isActive && `${styles.active}`;
 
 export const Navbar = () => {
   //get username from redux store
@@ -11,9 +15,32 @@ export const Navbar = () => {
     <div className={styles.navbar}>
       <div className={styles.user}>
         <FaUserCircle size={40} color="#fff" />
-        {userName}
+        <h4>{userName}</h4>
       </div>
-      <nav></nav>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/admin/home" className={activeLink}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/all-products" className={activeLink}>
+              All Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/add-product" className={activeLink}>
+              Add Product
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/orders" className={activeLink}>
+              Orders
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
