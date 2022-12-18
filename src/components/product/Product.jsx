0c8f9da5aeb616b1513da9f6c3171870de42cd48
_Allan <allankipchumba76@gmail.com>
2022-12-18 16:21:28 +0300

    created product component and sub-components

---
+++ src/components/product/Product.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./product.module.scss";
+
+export const Product = () => {
+  return <div>Product</div>;
+};
---
+++ src/components/product/productFilter/ProductFilter.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ProductFilter = () => {
+  return <div>ProductFilter</div>;
+};
