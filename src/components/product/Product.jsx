5903c85b596d513b8bf1b8d13105f53d4796ebf7
_Allan <allankipchumba76@gmail.com>
2022-12-18 16:33:46 +0300

    populated product component layout

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import styles from "./product.module.scss";
+import { ProductFilter } from "./productFilter/ProductFilter";
+import { ProductList } from "./productList/ProductList";
 
 export const Product = () => {
-  return <div>Product</div>;
+  return (
+    <section>
+      <div className={`container ${styles.product}`}>
+        <aside className={styles.filter}>
+          <ProductFilter />
+        </aside>
+        <div className={styles.content}>
+          <ProductList />
+        </div>
+      </div>
+    </section>
+  );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Product } from "../../components/product/Product";
 import { Slider } from "../../components/slider/Slider";
 
 export const Home = () => {
   return (
     <div>
       {/* <Slider /> */}
-      <h1>Home</h1>
+      <Product />
     </div>
   );
 };
