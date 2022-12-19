fb889dd8f09789b0e38189664f5f4a89ac2c9b8d
_Allan <allankipchumba76@gmail.com>
2022-12-19 16:42:17 +0300

    Show loading spinners when fetching product

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -2,9 +2,11 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useFetchCollection } from "../../customHooks/useFetchCollection";
 import { STORE_PRODUCTS } from "../../redux/slice/productSlice";
+import { Loader } from "../loader/Loader";
 import styles from "./product.module.scss";
 import { ProductFilter } from "./productFilter/ProductFilter";
 import { ProductList } from "./productList/ProductList";
+import spinnerImg from "../../assets/spinner.jpg";
 
 const { log } = console;
 
@@ -27,10 +29,22 @@ export const Product = () => {
     <section>
       <div className={`container ${styles.product}`}>
         <aside className={styles.filter}>
-          <ProductFilter />
+          {!isLoading && <ProductFilter />}
         </aside>
         <div className={styles.content}>
-          <ProductList products={products} />
+          {isLoading ? (
+            <>
+              {/* <img
+              src={spinnerImg}
+              alt="Loading..."
+              style={{ width: "50px" }}
+              className="--center-all"
+            /> */}
+              <Loader />
+            </>
+          ) : (
+            <ProductList products={products} />
+          )}
         </div>
       </div>
     </section>
