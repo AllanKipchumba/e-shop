7148bd2e66b86c1233b3cb230cf49505057ecdad
_Allan <allankipchumba76@gmail.com>
2022-12-19 07:44:56 +0300

    initiated productList component

--- src/components/product/productList/ProductList.jsx
+++ src/components/product/productList/ProductList.jsx
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./productList.module.scss";
+import { BsFillGridFill } from "react-icons/bs";
+import { FaListAlt } from "react-icons/fa";
 
 export const ProductList = () => {
-  return <div>ProductList</div>;
+  const [grid, setGrid] = useState(true);
+  return (
+    <div className={styles["product-list"]} id="product">
+      <div className={styles.top}>
+        <div className={styles.icons}>
+          <BsFillGridFill
+            size={22}
+            color="orangered"
+            onClick={() => setGrid(true)}
+          />
+          <FaListAlt
+            size={24}
+            color="orangered"
+            onClick={() => setGrid(false)}
+          />
+          <p>
+            <b>10 Products found.</b>
+          </p>
+        </div>
+
+        {/* search icon */}
+        <div>
+          <p>Search</p>
+        </div>
+
+        {/* sort products */}
+        <div className={styles.sort}>
+          <label>Sort by:</label>
+          <select>
+            <option value="latest">latest</option>
+            <option value="lowest-price">Lowest Price</option>
+            <option value="highest-price">Highest Price</option>
+            <option value="a-z">A - Z</option>
+            <option value="z-a">Z - A</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 };
