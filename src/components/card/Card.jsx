4d7bc59d00eef2fc64240e6784ecbdceea02106c
_Allan <allankipchumba76@gmail.com>
2022-12-19 14:06:14 +0300

    Fixed bug in card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Card.module.scss";
 
-export const Card = ({ children }) => {
-  return <div className={styles.card}>{children}</div>;
+export const Card = ({ children, cardClass }) => {
+  return <div className={`${styles.card} ${cardClass}`}>{children}</div>;
 };
