c21ba2785e112896cd5d909289407b6d10786a70
_Allan <allankipchumba76@gmail.com>
2022-12-21 06:38:59 +0300

    filter products by category

--- src/components/product/productFilter/ProductFilter.jsx
+++ src/components/product/productFilter/ProductFilter.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { FILTER_BY_CATEGORY } from "../../../redux/slice/filterSlice";
 import styles from "./productFilter.module.scss";
 
 export const ProductFilter = () => {
+  const [category, setCategory] = useState("All");
+  //get products from redux store
+  const { products } = useSelector((store) => store["product"]);
+
+  //create an array of categories
+  const allCategories = [
+    "All",
+    //look into all products and pick out unique caregories
+    ...new Set(products.map((product) => product.category)),
+  ];
+
+  //dispatch action to filter products by category
+  const dispatch = useDispatch();
+  const filterProducts = (cat) => {
+    setCategory(cat);
+    dispatch(FILTER_BY_CATEGORY({ products, category: cat }));
+  };
+
   return (
     <div className={styles.filter}>
       <h4>Categories</h4>
       <div className={styles.category}>
-        <button>All</button>
+        {allCategories.map((cat, index) => {
+          return (
+            <button
+              key={index}
+              type="button"
+              className={`${category}` === cat && `${styles.active}`}
+              onClick={() => filterProducts(cat)}
+            >
+              &#8250; {cat}
+            </button>
+          );
+        })}
       </div>
       <h4>Brand</h4>
       <div className={styles.brand}>
--- src/redux/slice/filterSlice.js
+++ src/redux/slice/filterSlice.js
@@ -48,15 +48,30 @@ const filterSlice = createSlice({
                         return b.name.localeCompare(a.name);
                     });
                     break;
+                    //default returns the latest product first
                 default:
                     tempProducts = products;
                     break;
             }
             state.filteredProduct = tempProducts;
         },
+        FILTER_BY_CATEGORY: (state, action) => {
+            const { products, category } = action.payload;
+            let tempProducts = [];
+            if (category === "All") {
+                tempProducts = products;
+            } else {
+                //return products whose category matches category requested by the user
+                tempProducts = products.filter(
+                    (product) => product.category === category
+                );
+            }
+            state.filteredProduct = tempProducts;
+        },
     },
 });
 
-export const { FILTER_BY_SEARCH, SORT_PRODUCTS } = filterSlice.actions;
+export const { FILTER_BY_SEARCH, SORT_PRODUCTS, FILTER_BY_CATEGORY } =
+filterSlice.actions;
 
 export default filterSlice.reducer;
