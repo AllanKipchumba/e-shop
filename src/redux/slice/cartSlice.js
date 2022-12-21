fcb148781c2eaccb118be789d35e5723a4aacbff
_Allan <allankipchumba76@gmail.com>
2022-12-21 17:15:22 +0300

    seted up redux cartSlice

---
+++ src/redux/slice/cartSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    //save in local storage to rehydrate store --preserve state after releoad
+
+    //Check if the value of cartItems exists in local storage. If it does, set the value of cartItems to the value stored in local storage
+    cartItems: localStorage.getItem("cartItems") //serialize cartItems
+        ?
+        JSON.parse(localStorage.getItem("cartItems")) :
+        [],
+    cartTotalQuantity: 0,
+    cartTotalAmount: 0,
+};
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        ADD_TO_CART: (state, action) => {
+            alert(action.payload);
+        },
+    },
+});
+
+export const { ADD_TO_CART } = cartSlice.actions;
+
+export default cartSlice.reducer;
