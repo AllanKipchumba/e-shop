b7d7c20c69ef621d0b3afb13cc60e02039f29bc3
_Allan <allankipchumba76@gmail.com>
2022-12-24 06:45:32 +0300

    handled checkout from the backend

--- server.js
+++ server.js
@@ -13,29 +13,48 @@ app.get("/", (req, res) => {
     res.send("Welcome to eShop website");
 });
 
-// const calculateOrderAmount = (items) => {
-//     // Replace this constant with a calculation of the order's amount
-//     // Calculate the order total on the server to prevent
-//     // people from directly manipulating the amount on the client
-//     return 1400;
-//   };
+const array = [];
+const calculateOrderAmount = (items) => {
+    items.map((item) => {
+        const { price, cartQuantity } = item;
+        const cartItemAmount = price * cartQuantity;
+        return array.push(cartItemAmount);
+    });
+    //return total amount
+    const totalAmount = array.reduce((a, b) => a + b, 0);
 
-// app.post("/create-payment-intent", async (req, res) => {
-//     const { items } = req.body;
+    return totalAmount * 100;
+};
 
-//     // Create a PaymentIntent with the order amount and currency
-//     const paymentIntent = await stripe.paymentIntents.create({
-//       amount: calculateOrderAmount(items),
-//       currency: "usd",
-//       automatic_payment_methods: {
-//         enabled: true,
-//       },
-//     });
+app.post("/create-payment-intent", async(req, res) => {
+    const { items, shipping, description } = req.body;
 
-//     res.send({
-//       clientSecret: paymentIntent.client_secret,
-//     });
-//   });
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: "usd",
+        automatic_payment_methods: {
+            enabled: true,
+        },
+        description,
+        shipping: {
+            address: {
+                line1: shipping.line1,
+                line2: shipping.line2,
+                city: shipping.city,
+                country: shipping.country,
+                postal_code: shipping.postal_code,
+            },
+            name: shipping.name,
+            phone: shipping.phone,
+        },
+        //   receipt_email: customEmail
+    });
+
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
 
 const port = process.env.port || 4242;
 app.listen(port, () => console.log(`Node server listening on port ${port}`));
