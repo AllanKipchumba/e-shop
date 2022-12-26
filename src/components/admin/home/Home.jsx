dc1dbc8bd0e75a677b4d88b485e8498fce0ec8ee
_Allan <allankipchumba76@gmail.com>
2022-12-26 12:09:30 +0300

    created InfoBox component, used it in admin home page

--- src/components/admin/home/Home.jsx
+++ src/components/admin/home/Home.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { InfoBox } from "../../index";
+import styles from "./home.module.scss";
+import { AiFillDollarCircle } from "react-icons/ai";
+import { BsCart4 } from "react-icons/bs";
+import { FaCartArrowDown } from "react-icons/fa";
+
+//icons
+const earningsIcon = <AiFillDollarCircle size={30} color="#b624ff" />;
+const productIcon = <BsCart4 size={30} color="#1f93ff" />;
+const ordersIcon = <FaCartArrowDown size={30} color="orangered" />;
 
 export const Home = () => {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className={styles.home}>
+      <h2>Admin Home</h2>
+      <div className={styles["info-box"]}>
+        <InfoBox
+          cardClass={`${styles.card} ${styles.card1}`}
+          title={"Earnings"}
+          count={"188"}
+          icon={earningsIcon}
+        />
+        <InfoBox
+          cardClass={`${styles.card} ${styles.card2}`}
+          title={"Products"}
+          count={"15"}
+          icon={productIcon}
+        />
+        <InfoBox
+          cardClass={`${styles.card} ${styles.card3}`}
+          title={"Orders"}
+          count={"50"}
+          icon={ordersIcon}
+        />
+      </div>
     </div>
   );
 };
--- src/components/infoBox/InfoBox.jsx
+++ src/components/infoBox/InfoBox.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Card } from "../card/Card";
+import styles from "./infoBox.module.scss";
 
-export const InfoBox = () => {
-  return <div>InfoBox</div>;
+export const InfoBox = ({ cardClass, title, count, icon }) => {
+  return (
+    <div className={styles["info-box"]}>
+      <Card cardClass={cardClass}>
+        <h4>{title}</h4>
+        <span>
+          <h3>{count}</h3>
+          {icon}
+        </span>
+      </Card>
+    </div>
+  );
 };
