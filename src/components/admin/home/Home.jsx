567282bfa68d352d231f3af77f37b1f04264ffe8
_Allan <allankipchumba76@gmail.com>
2022-12-26 12:57:44 +0300

    implemented infoComponent logic that fetchst data to popopulate the component

--- src/components/admin/home/Home.jsx
+++ src/components/admin/home/Home.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { InfoBox } from "../../index";
 import styles from "./home.module.scss";
 import { AiFillDollarCircle } from "react-icons/ai";
 import { BsCart4 } from "react-icons/bs";
 import { FaCartArrowDown } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { useFetchCollection } from "../../../customHooks/useFetchCollection";
+import { STORE_PRODUCTS } from "../../../redux/slice/productSlice";
+import {
+  CALC_TOTAL_ORDER_AMOUNT,
+  STORE_ORDERS,
+} from "../../../redux/slice/orderSlice";
 
 //icons
 const earningsIcon = <AiFillDollarCircle size={30} color="#b624ff" />;
@@ -11,6 +18,24 @@ const productIcon = <BsCart4 size={30} color="#1f93ff" />;
 const ordersIcon = <FaCartArrowDown size={30} color="orangered" />;
 
 export const Home = () => {
+  //access products and orders from firestore
+  const { data: firebaseProducts } = useFetchCollection("products");
+  const { data } = useFetchCollection("orders");
+
+  const dispatch = useDispatch();
+  //dispatch actions to store products and orders in redux and to calculate the total order amount
+  useEffect(() => {
+    dispatch(STORE_PRODUCTS({ products: firebaseProducts }));
+    dispatch(STORE_ORDERS(data));
+    dispatch(CALC_TOTAL_ORDER_AMOUNT());
+  }, [dispatch, firebaseProducts, data]);
+
+  //access products and orders from redux store
+  const { products } = useSelector((store) => store["product"]);
+  const { orderHistory: orders, totalOrderAmount } = useSelector(
+    (store) => store["orders"]
+  );
+
   return (
     <div className={styles.home}>
       <h2>Admin Home</h2>
@@ -18,19 +43,19 @@ export const Home = () => {
         <InfoBox
           cardClass={`${styles.card} ${styles.card1}`}
           title={"Earnings"}
-          count={"188"}
+          count={`$${totalOrderAmount}`}
           icon={earningsIcon}
         />
         <InfoBox
           cardClass={`${styles.card} ${styles.card2}`}
-          title={"Products"}
-          count={"15"}
+          title={`Products`}
+          count={products.length}
           icon={productIcon}
         />
         <InfoBox
           cardClass={`${styles.card} ${styles.card3}`}
           title={"Orders"}
-          count={"50"}
+          count={orders.length}
           icon={ordersIcon}
         />
       </div>
--- src/redux/slice/orderSlice.js
+++ src/redux/slice/orderSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     orderHistory: [],
+    totalOrderAmount: null,
 };
 
 const orderSlice = createSlice({
@@ -11,9 +12,21 @@ const orderSlice = createSlice({
         STORE_ORDERS: (state, action) => {
             state.orderHistory = action.payload;
         },
+        CALC_TOTAL_ORDER_AMOUNT: (state) => {
+            const array = [];
+            state.orderHistory.map((item) => {
+                const { orderAmount } = item;
+                return array.push(orderAmount);
+            });
+            //return total amount
+            const totalAmount = array.reduce((a, b) => a + b, 0);
+
+            //update state
+            state.totalOrderAmount = totalAmount;
+        },
     },
 });
 
-export const { STORE_ORDERS } = orderSlice.actions;
+export const { STORE_ORDERS, CALC_TOTAL_ORDER_AMOUNT } = orderSlice.actions;
 
 export default orderSlice.reducer;
